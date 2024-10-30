65d79cd76df52f712cfd582a86ed3afaae4ed35b
Viraj Dey <viraj65.dey@gmail.com>
2024-10-30 22:03:02 +0530

    left side bar

---
+++ chatapp/src/components/LeftSidebar/LeftSidebar.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './LeftSidebar.css'
+import assets from '../../assets/assets'
+const LeftSidebar = () => {
+  return (
+    <div className='ls'>
+      <div className="ls-top">
+        <div className="ls-nav">
+          <img src={assets.logo} className='logo' alt='' />
+          <div className="menu">
+            <img src={assets.menu_icon} alt="" />
+          </div>
+        </div>
+        <div className="ls-search">
+          <img src={assets.search_icon} alt="" />
+          <input type="text" placeholder='Search Here' />
+        </div>
+      </div>
+      <div className="ls-list">
+        <div className="friends">
+          <img src={assets.profile_img} alt="" srcset="" />
+          <div>
+            <p>Richard Sanford</p>
+            <span>Hello, How Are You?</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LeftSidebar
