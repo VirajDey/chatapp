13643839478372c7562cf7a333a67ec44b2f1b68
Viraj Dey <viraj65.dey@gmail.com>
2024-10-31 22:16:44 +0530

    setting db

---
+++ chatapp/src/config/firebase.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyD7HVT5Kaw9RzjHceq6x1EMU7u8XDkZjlw",
+    authDomain: "chat-app-ab1ad.firebaseapp.com",
+    projectId: "chat-app-ab1ad",
+    storageBucket: "chat-app-ab1ad.appspot.com",
+    messagingSenderId: "89952183289",
+    appId: "1:89952183289:web:cecb7abf55b447002c5328"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+        await setDoc(doc(db, "users", user.uid), {
+            id: user.uid,
+            username: username.toLowerCase(),
+            email,
+            name: "",
+            avatar: "",
+            bio: "Hey There I am using Chat App By Viraj",
+            lastseen: Date.now()
+        })
+        await setDoc(doc(db, "chats", user.uid), {
+            chatData: []
+        })
+    } catch (error) {
+        console.error(error)
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+const login = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+    } catch (error) {
+        console.error(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const logout = async () => {
+    try {
+        await signOut(auth)
+    } catch (error) {
+        console.error(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+export { signup, login, logout,auth,db}
--- chatapp/src/pages/login/login.jsx
+++ chatapp/src/pages/login/login.jsx
@@ -1,28 +1,41 @@
 import React, { useState } from 'react'
-import './login.css'
+import './Login.css'
 import assets from '../../assets/assets'
-const login = () => {
+import { signup,login } from '../../config/firebase'
+const Login = () => {
   const [currState, setCurrState] = useState("Sign Up");
+  const [userName, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (currState == "Sign Up") {
+      signup(userName,email,password);
+    }
+    else{
+      login(email,password)
+    }
+  }
 
   return (
     <div className='login'>
       <img src={assets.logo_big} alt="" className="logo" />
-      <form className="login_form">
+      <form onSubmit={onSubmitHandler} className="login_form">
         <h2>{currState}</h2>
-        {currState === "Sign Up" ? <input type='text' placeholder='Username' className="form-input" required /> : null}
-        <input type='email' placeholder='Email Address' className="form-input" required />
-        <input type='password' placeholder='Password' className="form-input" required />
+        {currState === "Sign Up" ? <input onChange={(e) => setUsername(e.target.value)} value={userName} type='text' placeholder='Username' className="form-input" required /> : null}
+        <input onChange={(e) => setEmail(e.target.value)} value={email} type='email' placeholder='Email Address' className="form-input" required />
+        <input onChange={(e) => setPassword(e.target.value)} value={password} type='password' placeholder='Password' className="form-input" required />
         <button type='submit'>{currState === "Sign Up" ? "Create Account" : "Login Now"}</button>
         <div className="login-term">
           <input type="checkbox" />
           <p>Agree to the terms of use and Privacy Policy</p>
         </div>
         <div className="login-forget">
-          {currState === "Sign Up"?<p className="login-toggle">Alredy have an account <span onClick={() => setCurrState("Login")}>Login Here</span></p>:<p className="login-toggle">Create an Account <span onClick={() => setCurrState("Sign Up")}>Click Here</span></p>}
-    </div>
+          {currState === "Sign Up" ? <p className="login-toggle">Alredy have an account <span onClick={() => setCurrState("Login")}>Login Here</span></p> : <p className="login-toggle">Create an Account <span onClick={() => setCurrState("Sign Up")}>Click Here</span></p>}
+        </div>
       </form >
     </div >
   )
 }
 
-export default login
+export default Login
