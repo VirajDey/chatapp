1cbbc089fa44bbaf50d5c3d895412b59f6405904
Viraj Dey <viraj65.dey@gmail.com>
2024-11-01 19:14:01 +0530

    2:59:40

--- chatapp/src/context/AppContext.jsx
+++ chatapp/src/context/AppContext.jsx
@@ -1,28 +1,47 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { createContext, useState } from "react";
-import { db } from "../config/firebase";
+import { auth, db } from "../config/firebase";
+import { useNavigate } from 'react-router-dom'
+
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
 
-    const [userData,setUserData] = useState(null);
-    const [chatData,setChatData] = useState(null);
+    const navigate = useNavigate();
+    const [userData, setUserData] = useState(null);
+    const [chatData, setChatData] = useState(null);
 
-    const loadUserData = async (uid) =>{
+    const loadUserData = async (uid) => {
         try {
-            const userRef = doc(db,"users",uid);
+            const userRef = doc(db, "users", uid);
             const userSnap = await getDoc(userRef);
             const userData = userSnap.data();
-            console.log(userData);
+            setUserData(userData);
+            if (userData.avatar && userData.name) {
+                navigate('/chat')
+            }
+            else {
+                navigate('/profile')
+            }
+            await updateDoc(userRef, {
+                lastSeen: Date.now()
+            })
+            setInterval(async () => {
+                if (auth.chatUser) {
+                    await updateDoc(userRef, {
+                        lastSeen: Date.now()
+                    })
+                }
+            }, 60000)
         } catch (error) {
-            
+
         }
     }
 
     const value = {
-        userData,setChatData,
-        chatData,setChatData,
+        userData, setUserData,
+        chatData, setChatData,
         loadUserData
     }
 
--- chatapp/src/pages/profileUpdate/ProfileUpdate.jsx
+++ chatapp/src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,9 +1,42 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './config/firebase'
+import { doc, getDoc } from 'firebase/firestore'
+import { db } from '../../config/firebase'
+import { useNavigate } from 'react-router-dom'
+
 const ProfileUpdate = () => {
 
+  const navigate = useNavigate();
   const [image, setImage] = useState(false);
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [uid, setUid] = useState("");
+  const [prevImage, setPrevImage] = useState("");
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        setUid(user.uid)
+        const docRef = doc(db, 'users', user.uid)
+        const docSnap = await getDoc(docRef)
+        if (docSnap.data().name) {
+          setName(docSnap.data().name);
+        }
+        if (docSnap.data().bio) {
+          setBio(docSnap.data().bio);
+        }
+        if (docSnap.data().avatar) {
+          setPrevImage(docSnap.data().avatar);
+        }
+      }
+      else {
+        navigate('/')
+      }
+    })
+  }, [])
 
   return (
     <div className='profile'>
@@ -12,14 +45,14 @@ const ProfileUpdate = () => {
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
             <input onChange={(e) => setImage(e.target.files[0])} type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden />
-            <img src={image? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
+            <img src={image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
             upload profile image
           </label>
-          <input type="text" placeholder='Your Name' required />
-          <textarea placeholder='Write Profile Bio' required></textarea>
+          <input onChange={(e) => { setName(e.target.value) }} value={name} type="text" placeholder='Your Name' required />
+          <textarea onChange={(e) => { setBio(e.target.value) }} value={bio} placeholder='Write Profile Bio' required></textarea>
           <button type="submit">Save</button>
         </form>
-        <img className='profile-pic' src={image? URL.createObjectURL(image) : assets.logo_icon} alt="" />
+        <img className='profile-pic' src={image ? URL.createObjectURL(image) : assets.logo_icon} alt="" />
       </div>
     </div>
   )
