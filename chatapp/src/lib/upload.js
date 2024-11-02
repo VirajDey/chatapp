cdeb9999637b430a920ebb08a834df33edce4c34
Viraj Dey <viraj65.dey@gmail.com>
2024-11-03 00:15:30 +0530

    bug-fixing, 3:12:10

--- chatapp/src/lib/upload.js
+++ chatapp/src/lib/upload.js
@@ -6,32 +6,33 @@ const upload = async (file) => {
 
     const uploadTask = uploadBytesResumable(storageRef, file);
 
+    return new Promise((resolve, reject) => {
+        uploadTask.on('state_changed',
+            (snapshot) => {
 
-    uploadTask.on('state_changed',
-        (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                console.log('Upload is ' + progress + '% done');
+                switch (snapshot.state) {
+                    case 'paused':
+                        console.log('Upload is paused');
+                        break;
+                    case 'running':
+                        console.log('Upload is running');
+                        break;
+                }
+            },
+            (error) => {
 
-            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            console.log('Upload is ' + progress + '% done');
-            switch (snapshot.state) {
-                case 'paused':
-                    console.log('Upload is paused');
-                    break;
-                case 'running':
-                    console.log('Upload is running');
-                    break;
+            },
+            () => {
+
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL)
+                });
             }
-        },
-        (error) => {
-            // Handle unsuccessful uploads
-        },
-        () => {
-            // Handle successful uploads on complete
-            // For instance, get the download URL: https://firebasestorage.googleapis.com/...
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                resolve(downloadURL)
-            });
-        }
-    );
+        );
+    })
+
 }
 
 export default upload;
--- chatapp/src/pages/profileUpdate/ProfileUpdate.jsx
+++ chatapp/src/pages/profileUpdate/ProfileUpdate.jsx
@@ -1,11 +1,14 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './ProfileUpdate.css'
 import assets from '../../assets/assets'
 import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from './config/firebase'
-import { doc, getDoc } from 'firebase/firestore'
+import { auth } from '../../config/firebase'
+import { doc, getDoc, updateDoc } from 'firebase/firestore'
 import { db } from '../../config/firebase'
 import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import upload from '../../lib/upload'
+import { AppContext } from '../../context/AppContext'
 
 const ProfileUpdate = () => {
 
@@ -15,13 +18,45 @@ const ProfileUpdate = () => {
   const [bio, setBio] = useState("");
   const [uid, setUid] = useState("");
   const [prevImage, setPrevImage] = useState("");
+  const {setUserData} = useContext(AppContext)
+
+  const profileUpdate = async (event) => {
+    event.preventDefault();
+    try {
+      if (!prevImage && !image) {
+        toast.error("Uplaod Profile Picture")
+      }
+      const docRef = doc(db, 'users', uid)
+      if (image) {
+        const imgUrl = await upload(image);
+        setPrevImage(imgUrl);
+        await updateDoc(docRef, {
+          avatar: imgUrl,
+          bio: bio,
+          name: name
+        })
+      }
+      else {
+        await updateDoc(docRef, {
+          bio: bio,
+          name: name
+        })
+      }
+      const snap = await getDoc(docRef);
+      setUserData(snap.data());
+      navigate('/chat');
+    } catch (error) {
+      console.error(error);
+      toast.error(error.message);
+    }
+  }
 
   useEffect(() => {
     onAuthStateChanged(auth, async (user) => {
       if (user) {
         setUid(user.uid)
-        const docRef = doc(db, 'users', user.uid)
-        const docSnap = await getDoc(docRef)
+        const docRef = doc(db, 'users', user.uid);
+        const docSnap = await getDoc(docRef); 
         if (docSnap.data().name) {
           setName(docSnap.data().name);
         }
@@ -41,15 +76,15 @@ const ProfileUpdate = () => {
   return (
     <div className='profile'>
       <div className="profile-container">
-        <form >
+        <form onSubmit={profileUpdate}>
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
             <input onChange={(e) => setImage(e.target.files[0])} type="file" id="avatar" accept='.png, .jpg, .jpeg' hidden />
             <img src={image ? URL.createObjectURL(image) : assets.avatar_icon} alt="" />
             upload profile image
           </label>
-          <input onChange={(e) => { setName(e.target.value) }} value={name} type="text" placeholder='Your Name' required />
-          <textarea onChange={(e) => { setBio(e.target.value) }} value={bio} placeholder='Write Profile Bio' required></textarea>
+          <input onChange={(e) =>  setName(e.target.value) } value={name} type="text" placeholder='Your Name' required />
+          <textarea onChange={(e) =>  setBio(e.target.value) } value={bio} placeholder='Write Profile Bio' required></textarea>
           <button type="submit">Save</button>
         </form>
         <img className='profile-pic' src={image ? URL.createObjectURL(image) : assets.logo_icon} alt="" />
